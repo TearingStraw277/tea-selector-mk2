703f5055a98a43509f346205738cddbf4147875d
Lewis Harrison <l.harrison06@outlook.com>
2023-11-16 21:51:11 +0000

    testing navbar

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,22 @@
-"use client"
-
+const navigation = [
+  {name: 'Home', href: '.', current: false},
+  {name: 'Black', href: '#', current: false},
+  {name: 'Green', href: '#', current: false},
+  {name: 'Misc', href: '#', current: false},
+]
 export default function Home(){
-
-  function handleClick(){
-    window.open("./python/test.py")
-  }
   return(
-    <div>
-      <button onClick={handleClick}>Work</button>
-    </div>
+    <div className="flex space-x-4">
+      {navigation.map((item) => (
+        <a
+          key={item.name}
+          href={item.href}
+          className="(item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white','rounded-md px-3 py-2 text-sm font-medium')"
+          aria-current={item.current ? 'page':undefined}
+        >
+          {item.name}
+        </a>
+      ))}
+      </div>
   )
 }
