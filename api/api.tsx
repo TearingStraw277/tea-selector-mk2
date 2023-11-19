357573b045393713bb393a48b3c50219499939d8
Lewis Harrison <l.harrison06@outlook.com>
2023-11-19 10:00:38 +0000

    data test

---
+++ api/api.tsx
@@ -0,0 +1,11 @@
+async function Data() {
+    const res = [
+        {Name:'Earl Grey', Description:'Early', Type:'Black', group:'Breakfast', href:''},
+        {Name:'Assam', Description:'assam', Type:'Black', group:'Breakfast', href:''},
+        {Name:'Darjeeling', Description:'darj', Type:'Black', group:'Breakfast', href:''},
+        {Name:'Jasmine', Description:'', Type:'Green', group:'Flowering', href:''},
+    ]
+    return res;
+}
+
+module.exports = {Data}
