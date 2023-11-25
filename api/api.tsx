55c1846460dc864f0712cd42b49953db33ebdb5b
Lewis Harrison <l.harrison06@outlook.com>
2023-11-25 21:35:18 +0000

    crud

--- api/api.tsx
+++
@@ -1,9 +0,0 @@
-const sqlite3 = require('sqlite3').verbose();
-
-// open the database
-let db = new sqlite3.Database('../data/tea_db.db', sqlite3.OPEN_READWRITE, (err) => {
-    if (err) {
-        console.error(err.message);
-    }
-    console.log('Connected to the chinook database.');
-});
--- app/pages/new/page.tsx
+++ app/pages/new/page.tsx
@@ -2,31 +2,14 @@
 
 import NavBar from "@/app/components/navbar"
 
-export default function New(){
+export default function Home(){
 
-    function handleClick(){
-        alert('Clicked')
-    }
+
 
     return(
         <>
-            <div><NavBar placeholder='Search'></NavBar></div>
-
             <div>
-                <form className="shadow">
-                    <div>
-                        <label>Name</label>
-                        <input placeholder="Name"/>
-                    </div>
-                    <div>
-                        <label>Dropdown</label>
-                        <select>
-                            <option>Black</option>
-                            <option>Green</option>
-                            <option>Misc</option>
-                        </select>
-                    </div>
-                </form>
+                <NavBar placeholder="Search"></NavBar>
             </div>
         </>
     )
